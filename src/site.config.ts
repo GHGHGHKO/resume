3844fe5480c7ddf41fad5d761f7ecbe69dff67b5
GHGHGHKO <gudrb963@gmail.com>
2025-05-26 00:43:39 +0900

    feat: change from srleom to feelsgoodfrog

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'feelsgoodfrog',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: 'feelsgoodfrog resume',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'feelsgoodfrog resume',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
