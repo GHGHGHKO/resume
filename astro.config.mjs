6ae491696a50b29ce0689409628e5d4ef4ce4f26
GHGHGHKO <gudrb963@gmail.com>
2025-06-08 19:16:16 +0900

    chore: change defineConfig site

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://feelsgoodfrog.vercel.app',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
